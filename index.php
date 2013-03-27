b022ec3fcf856fd2ad94b3049d64c5afac8e9f8a
danferth <danferth@gmail.com>
2013-03-27 14:12:49 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>.::|FRIDAY|::.</title>
+</head>
+<body>
+	<div class="taco">
+		<div class="countdown">
+			10:00:35
+		</div>
+		<p class="tag">Firday will come please be patient<span>:)</span></p>
+	</div>
+</body>
+</html>
