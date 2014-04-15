0c6a2279a995c856fbb2dea614bde3ca51987450
danferth <danferth@gmail.com>
2014-04-15 13:29:13 -0700

    removed footer tag

--- theme/foot.php
+++ theme/foot.php
@@ -1,7 +1,3 @@
-<footer>
-         <p>footer data</p>
-</footer>  
-
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="/assets/js/lib/jquery-1.10.1.min.js"><\/script>')</script>
         <script src="/assets/js/build/production.min.js"></script>
