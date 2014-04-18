071df707da1d5dec4f2fc7fa4a22f332d8a35abd
danferth <danferth@gmail.com>
2014-04-18 12:08:12 -0700

    bug hunt continues

--- index.php
+++ index.php
@@ -6,7 +6,9 @@ $body->head();
 ?>
 
 <div class="taco">
-<h1 style="color:#fff;"><?php echo date(e) ?></h1>
+<h1 style="color:#fff;"><?php echo date(e); ?></h1>
+
+?>
 	<div class="timer">
 		<div class="countdown"></div>
 		<p class="tag">Friday <strong>5:30</strong> will come, please be patient&nbsp; &nbsp;<span>:o)</span></p>
