8fd5e7204e4d9bc7489fcdd195fcd2e0340c5784
Dan Klotz <danferth@gmail.com>
2015-08-22 00:35:12 +0000

    almost there

--- timer.js
+++ timer.js
@@ -1,5 +1,3 @@
-console.time("start");
-
 var now           = new Date(),
     now_day       = now.getDay(),
     now_month     = now.getMonth(),
@@ -41,14 +39,45 @@ var diff_days     = 0,
     diff_minutes  = 0,
     diff_seconds  = 0;
 
-
+//find seconds
 if(time_diff % 1000 === 0){
   diff_seconds = time_diff / 1000;
 }else{
-  var  n = time_diff % 1000;
-  diff_seconds = (time_diff - n) / 1000;
+  var remainder = time_diff % 1000;
+  diff_seconds = (time_diff - remainder)/1000;
 }
 
-console.log(diff_seconds);
+//pars function
+var find_time = function(time, count){
+  if(diff_seconds % count === 0){
+    time = diff_seconds / count;
+  }else{
+    var  n = diff_seconds % count;
+    time = (diff_seconds - n) / count;
+  }
+  diff_seconds = n;
+  return time;
+};
 
-console.timeEnd("start");
+var days = find_time(diff_days, 86400);
+var hours = find_time(diff_hours, 3600);
+var minutes = find_time(diff_minutes, 60);
+var seconds = diff_seconds;
+
+console.log("days "+days+ " | hours "+hours+" | minutes "+minutes+" | seconds "+seconds);
+
+var countdown = function(sec){
+  var start_seconds = seconds;
+  find_time(days, 86400);
+  find_time(hours, 3600);
+  find_time(minutes, 60);
+  seconds = diff_seconds;
+
+  console.log("days "+days+ " | hours "+hours+" | minutes "+minutes+" | seconds "+seconds);
+
+  start_seconds = seconds -1;
+};
+
+setInterval("countdown(seconds)", 1000);
+
+
